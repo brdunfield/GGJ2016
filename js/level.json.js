8274591f885fa5d84dc2d132bc67feb4efe6a794
Andrew Brough <spectrum1012@gmail.com>
2016-01-30 14:56:16 -0500

    Added grass, ground assets and ability to 'fill below' to fill until the bottom of screen

--- js/level.json.js
+++ js/level.json.js
@@ -1,30 +1,19 @@
 //level[x][y] = {"asset", "trigger"}
 var level = [];
-for (var i=0; i < 300; i++)
+for (var i = 0; i < 300; i++)
     level[i] = [];
 
-level[0][17] = {asset:"test"};
-level[1][17] = {asset:"test"};
-level[2][17] = {asset:"test"};
-level[3][17] = {asset:"test"};
-level[4][17] = {asset:"test"};
-level[5][17] = {asset:"test"};
-level[6][17] = {asset:"test"};
-level[7][17] = {asset:"test"};
-level[8][17] = {asset:"test"};
-level[9][17] = {asset:"test"};
-level[10][17] = {asset:"test"};
-level[10][17] = {asset:"test"};
-level[11][17] = {asset:"test"};
-level[12][17] = {asset:"test"};
-level[13][17] = {asset:"test"};
-level[14][17] = {asset:"test"};
-level[15][17] = {asset:"test"};
-level[16][17] = {asset:"test"};
-level[17][17] = {asset:"test"};
-level[18][17] = {asset:"test"};
-level[19][17] = {asset:"test"};
-
-level[11][14] = {asset:"test"};
-level[12][14] = {asset:"test"};
-level[13][14] = {asset:"test"};
+// --- Area 1 ---
+//ground
+for (var x = 0; x < 200; x++) {
+    level[x][17] = {
+        asset: "grass",
+        fillBelowTile: "ground"
+    };
+}
+//platforms
+for (var x = 11; x < 14; x++) {
+    level[x][14] = {
+        asset: "grass"
+    };
+}
